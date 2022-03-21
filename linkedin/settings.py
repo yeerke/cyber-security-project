55d33a46ad41c0426383052a2b1d5d6fce7018b2
yeerke <ezhumalin01@gmail.com>
2022-03-21 13:15:10 +0600

    added html template

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a@%7$-=8+u%#asol-=yo4+sg)obhdpwqw99-9*uj(wc(b97vik
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.linkedlm.com']
+ALLOWED_HOSTS = ['www.linkedlm.com', '127.0.0.1']
 
 
 # Application definition
--- main/views.py
+++ main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('hello')
+    return render(request, 'sign-in.html')
